9a9a01a40f1b95b9fc846e2d8a01b7029774f5ca
chinathaip <cartoonabe@gmail.com>
2025-01-18 01:41:34 +0700

    feat: adjust status code on success

--- src/elysia/controllers/webhook.ts
+++ src/elysia/controllers/webhook.ts
@@ -3,23 +3,26 @@ import { BaseClerkWebhook, ClerkOrganizationWebhook } from "@/elysia/types/clerk
 import { softDeleteOrganization, upsertOrganization } from "@/elysia/services/organization";
 import { ClerkWebhookEvent } from "../utils/const";
 
-export const webhookController = new Elysia({ prefix: "/webhook" }).post(
-	"/clerk",
-	({ body }: { body: { data: BaseClerkWebhook<any> } }) => {
-		switch (body.data.type) {
-			case ClerkWebhookEvent.ORGANIZATION_CREATED:
-			case ClerkWebhookEvent.ORGANIZATION_UPDATED:
-				return upsertOrganization(body.data.data as ClerkOrganizationWebhook).catch((err) => {
+export const webhookController = new Elysia({ prefix: "/webhook" }).post("/clerk", ({ body, set }) => {
+	const webhook = body as BaseClerkWebhook<any>;
+	switch (webhook.type) {
+		case ClerkWebhookEvent.ORGANIZATION_CREATED:
+		case ClerkWebhookEvent.ORGANIZATION_UPDATED:
+			return upsertOrganization(webhook.data as ClerkOrganizationWebhook)
+				.then(() => {
+					set.status = webhook.type == ClerkWebhookEvent.ORGANIZATION_CREATED ? 201 : 200;
+					return "Ok";
+				})
+				.catch((err) => {
 					console.error(err);
-					return error("Internal Server Error", "something wrong on our end while upserting org");
+					return error(500, "something wrong on our end while upserting org");
 				});
-			case ClerkWebhookEvent.ORGANIZATION_DELETED:
-				return softDeleteOrganization(body.data.data as ClerkOrganizationWebhook).catch((err) => {
-					console.error(err);
-					return error("Internal Server Error", "something wrong on our end while deleting org");
-				});
-			default:
-				return error("Bad Request", "unsupported event");
-		}
-	},
-);
+		case ClerkWebhookEvent.ORGANIZATION_DELETED:
+			return softDeleteOrganization(webhook.data as ClerkOrganizationWebhook).catch((err) => {
+				console.error(err);
+				return error(500, "something wrong on our end while deleting org");
+			});
+		default:
+			return error(400, "unsupported event");
+	}
+});
