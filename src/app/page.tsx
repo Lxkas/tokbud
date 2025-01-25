74defb8b23b6be56d9727780f8634c5943a46956
Lxkas <alohasunshineday@yahoo.com>
2025-01-25 14:13:33 +0700

    setup with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { elysia } from "@/elysia/client";
-
 export default async function Home() {
-	const { data, error } = await elysia.api.message.index.get();
-
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<p className={`text-4xl font-bold ${error != null ? "text-red-700" : ""}`}>{(error?.value as string) ?? data}</p>
+		<main>
+			<p>Hello world!</p>
 		</main>
 	);
 }
