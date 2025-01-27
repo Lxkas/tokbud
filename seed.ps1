d1c5c16821a5a5ee3783fd51a7f27b6340cf7fab
chinathaip <cartoonabe@gmail.com>
2025-01-27 20:59:25 +0700

    feat: include test script

--- seed.ps1
+++ seed.ps1
@@ -10,16 +10,20 @@ if (-not (Get-Command aws -ErrorAction SilentlyContinue)) {
     exit 1
 }
 
-elasticdump --input=es_dump\time_record_mapping.json --output=http://localhost:9200/time_record --type=mapping
-elasticdump --input=es_dump\time_record.json --output=http://localhost:9200/time_record --type=data
+elasticdump --input=testdata\es_dump\time_record_mapping.json --output=http://localhost:9200/time_record --type=mapping
+elasticdump --input=testdata\es_dump\time_record.json --output=http://localhost:9200/time_record --type=data
 
-elasticdump --input=es_dump\organization_mapping.json --output=http://localhost:9200/organization --type=mapping
-elasticdump --input=es_dump\organization.json --output=http://localhost:9200/organization --type=data
+elasticdump --input=testdata\es_dump\organization_mapping.json --output=http://localhost:9200/organization --type=mapping
+elasticdump --input=testdata\es_dump\organization.json --output=http://localhost:9200/organization --type=data
+
+Invoke-WebRequest -Uri "http://localhost:9200/organization/_search" -Method Get | Select-Object -ExpandProperty Content
+Invoke-WebRequest -Uri "http://localhost:9200/time_record/_search" -Method Get | Select-Object -ExpandProperty Content
 
 $env:AWS_ACCESS_KEY_ID = "normal"
 $env:AWS_SECRET_ACCESS_KEY = "normal"
 $env:AWS_DEFAULT_REGION = "wtf-2"
 
 aws s3 cp --recursive .\testdata\images\ s3://tokbud/time-record --endpoint-url http://localhost:8333
+aws s3 cp s3://tokbud/time-record/clock-in1.jpeg .\test1.jpeg --endpoint-url http://localhost:8333
 
 Write-Host "Done"
--- seed.sh
+++ seed.sh
@@ -13,16 +13,18 @@ then
 fi
 
 
-elasticdump --input=es_dump/time_record_mapping.json --output=http://localhost:9200/time_record --type=mapping
-elasticdump --input=es_dump/time_record.json --output=http://localhost:9200/time_record --type=data
+elasticdump --input=testdata/es_dump/time_record_mapping.json --output=http://localhost:9200/time_record --type=mapping
+elasticdump --input=testdata/es_dump/time_record.json --output=http://localhost:9200/time_record --type=data
 
-elasticdump --input=es_dump/organization_mapping.json --output=http://localhost:9200/organization --type=mapping
-elasticdump --input=es_dump/organization.json --output=http://localhost:9200/organization --type=data
+elasticdump --input=testdata/es_dump/organization_mapping.json --output=http://localhost:9200/organization --type=mapping
+elasticdump --input=testdata/es_dump/organization.json --output=http://localhost:9200/organization --type=data
 
+curl http://localhost:9200/organization/_search
+curl http://localhost:9200/time_record/_search
  
 export AWS_ACCESS_KEY_ID=normal
 export AWS_SECRET_ACCESS_KEY=normal
 export AWS_DEFAULT_REGION=wtf-2
 
 aws s3 cp --recursive ./testdata/images/ s3://tokbud/time-record --endpoint-url http://localhost:8333
-echo "Done"
+aws s3 cp s3://tokbud/time-record/clock-in1.jpeg ./test1.jpeg --endpoint-url http://localhost:8333
